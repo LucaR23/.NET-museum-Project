3068b11bb50160ac2892dfe5f451af13528f5bc5
LucaRocci <100279349+LucaRocci@users.noreply.github.com>
2022-11-14 20:25:33 +0100

    modifiche amministratore e js vario

--- WebAppMupin/WebAppMupin/Controllers/AdministratorController.cs
+++ WebAppMupin/WebAppMupin/Controllers/AdministratorController.cs
@@ -3,10 +3,12 @@ using Renci.SshNet.Security.Cryptography;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
+using WebAppMupin.Models;
 
 namespace WebAppMupin.Controllers
 {
@@ -16,7 +18,7 @@ namespace WebAppMupin.Controllers
         public ActionResult Index()
         {
             MySqlConnection cnn = UtilityDB.connection();
-            string query = "SELECT Nome,Cognome,username FROM utenti WHERE Abilitato=1 AND DefaultPassword!=password;";
+            string query = "SELECT Nome,Cognome,username,Abilitato FROM utenti WHERE password IS NULL OR DefaultPassword!=password;";
             DataTable dt = UtilityDB.GetDataTable(query, cnn);
             return View("HomeAdministrator",dt);
         }
@@ -30,7 +32,7 @@ namespace WebAppMupin.Controllers
         public ActionResult Reset(string username)
         {
             MySqlConnection cnn = UtilityDB.connection();
-            string query = "UPDATE `utenti` SET `password`=NULL WHERE `username`=@us";
+            string query = "UPDATE `utenti` SET `password`=NULL,`Abilitato`=0  WHERE `username`=@us";
             MySqlCommand cmd = cnn.CreateCommand();
             cmd.CommandText = query;
             cmd.Parameters.AddWithValue("@us", username);
@@ -40,6 +42,20 @@ namespace WebAppMupin.Controllers
             cnn.Close();
             return RedirectToAction("Index", "administrator");
 
+        }
+
+        public ActionResult Enable(string username)
+        {
+            MySqlConnection cnn = UtilityDB.connection();
+            string query = "UPDATE `utenti` SET `Abilitato`=1 WHERE `username`=@us";
+            MySqlCommand cmd = cnn.CreateCommand();
+            cmd.CommandText = query;
+            cmd.Parameters.AddWithValue("@us", username);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+            return RedirectToAction("Index", "administrator");
         }
 
         public ActionResult DeleteUser(string username)
@@ -54,6 +70,46 @@ namespace WebAppMupin.Controllers
             cmd.ExecuteNonQuery();
             cnn.Close();
             return RedirectToAction("Index", "administrator");
+        }
+
+        public ActionResult New()
+        {
+            NewUser newUser = new NewUser();
+            newUser.DefaultPassword = "admin";
+            newUser.message = "Inserisci i dati necessari";   
+            return PartialView("_NewUser", newUser);
+        }
+
+        public bool Insert(NewUser n)
+        {
+            if (n.UserName == null || n.Nome == null || n.Cognome == null)
+            {
+                n.message = "Compila tutti i campi";
+                return false;
+            }
+            // controllare di non inserire due username uguali
+            else
+            {
+                MySqlConnection cnnn = UtilityDB.connection();
+                string query = "INSERT INTO `utenti`(`username`,`DefaultPassword`, `Nome`, `Cognome`) VALUES (@us,@def,@nome,@cgn)";
+                MySqlCommand cmd = new MySqlCommand(query, cnnn);
+                cmd.Parameters.AddWithValue("@us", n.UserName);
+                cmd.Parameters.AddWithValue("@def","admin");
+                cmd.Parameters.AddWithValue("@nome", n.Nome);
+                cmd.Parameters.AddWithValue("@cgn", n.Cognome);
+                try
+                {
+                    cnnn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch(MySqlException ex)
+                {
+                    return false;
+                }
+                cnnn.Close();
+                n.message = "nuovo Utente inserito";
+                return true;
+            }
         }
     }
 }
---
+++ WebAppMupin/WebAppMupin/Models/NewUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMupin.Models
+{
+    public class NewUser
+    {
+        public string UserName { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string DefaultPassword { get; set; }
+        public string message { get; set; } 
+
+    }
+}
